0fb5543c3cd0bd0bae85564e39560016cc5facbf
Frédéric van der Essen <fvdessen@gmail.com>
2013-07-29 02:04:43 +0200

    wip

--- src/js/main.js
+++ src/js/main.js
@@ -1,40 +1,71 @@
-if(window.location.hash){
-    $('html').html('');
-    window.stop();
+(function(){
 
-    var doc = window.location.hash.slice(1);
-    if(doc[0] === '?'){
+    function ask_for_password(pw_callback){
         $('html').html("<input type='text' placeholder='Password' id='password'></input><button id='decrypt'>Decrypt</button>");
         $('#decrypt').click(function(){
-            doc = doc.slice(1);
-            doc = CryptoJS.AES.decrypt(doc,$('#password').val()).toString(CryptoJS.enc.Utf8);
+            var pw = $('#password').val();
+            if(pw){
+                pw_callback(pw);
+            }
+        });
+    }
+
+    function decode(doc){
+        $('html').html('');
+        window.stop();
+
+        if(doc[0] === '?'){
+            ask_for_password(function(password){
+                doc = doc.slice(1);
+                doc = CryptoJS.AES.decrypt(doc,password).toString(CryptoJS.enc.Utf8);
+                doc = new Int8Array(Base64Binary.decode(doc).buffer);
+                LZMA.decompress(doc, function(result){
+                    $('html').html(result);
+                });
+            });
+        }else{
             doc = new Int8Array(Base64Binary.decode(doc).buffer);
             LZMA.decompress(doc, function(result){
                 $('html').html(result);
             });
-        });
-    }else{
-        doc = new Int8Array(Base64Binary.decode(doc).buffer);
-        LZMA.decompress(doc, function(result){
-            $('html').html(result);
+        }
+    }
+
+    function encode_url(content,password,success){
+        LZMA.compress(content,5, function(result){
+            result = new Uint8Array((new Int8Array(result)).buffer);
+            var encoded = base64ArrayBuffer(result);
+            if(password){
+                encoded = CryptoJS.AES.encrypt(encoded,password).toString();
+                encoded = '?' + encoded;
+            }
+            success('/#'+encoded);
         });
     }
-}else{
-    $(function(){
+
+    function homepage(){
         $('#submit').click(function(){
             var content = $('#content').val();
             var password = $('#password').val();
-            if(!content){ return; }
-            LZMA.compress(content,5,function(result){
-                var ures = new Uint8Array((new Int8Array(result)).buffer);
-                var enc = base64ArrayBuffer(ures.buffer);
-                if(password){
-                    enc = CryptoJS.AES.encrypt(enc,password).toString();
-                    enc = '?' + enc;
-                }
-                window.location = '/#'+enc;
-                window.location.reload();
-            },function(progress){console.log(progress);});
+            if(content){ 
+                $('.loading').removeClass('hidden');
+                setTimeout(function(){
+                    encode_url(content,password,function(result){
+                        var url = window.location.origin + result;
+                        $('.urlbox .url').attr('href',url).text(url);
+                        $('.urlbox .js-url-length').text(url.length);
+                        $('.urlbox .js-url-encrypted').text( password ? 'Yes' : 'No');
+                        $('.urlbox').removeClass('hidden');
+                        $('.loading').addClass('hidden');
+                    });
+                },500);
+            }
         });
-    });
-}
+    }
+
+    if(window.location.hash){
+        decode(window.location.hash.slice(1));
+    }else{
+        $(function(){ homepage() });
+    }
+})();

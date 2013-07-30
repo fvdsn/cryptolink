481cf74778e105a9aaf47fa284a21669f524bf0e
Frédéric van der Essen <fvdessen@gmail.com>
2013-07-31 01:45:35 +0200

    expanding on faq and message type prototype

--- src/js/main.js
+++ src/js/main.js
@@ -44,13 +44,25 @@
     }
 
     function homepage(){
+
+        $('#content').focus();
+        
+        $('#content').bind('input propertychange',function(){
+            if($(this).val()){
+                $('#submit').removeClass('disabled');
+            }else{
+                $('#submit').addClass('disabled');
+            }
+            $('.urlbox').addClass('hidden');
+        });
+
         $('#submit').click(function(){
             var content = $('#content').val();
             var password = $('#password').val();
             if(content){ 
                 $('.loading').removeClass('hidden');
                 setTimeout(function(){
-                    encode_url(content,password,function(result){
+                    encode_url('<pre>'+content+'</pre>',password,function(result){
                         var url = window.location.origin + result;
                         $('.urlbox .url').attr('href',url).text(url);
                         $('.urlbox .js-url-length').text(url.length);

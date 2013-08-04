3abfbf497f7af9aa0369c65fa3e73515d26cb8d8
Frédéric van der Essen <fvdessen@gmail.com>
2013-08-04 18:28:13 +0200

    image editor, ace editor, password strength checker

--- src/js/gui.js
+++ src/js/gui.js
@@ -1,24 +1,49 @@
 $(function(){
 
-    var content_type = '';  
-    
-    function set_content_type(type){
-        console.log('set_content_type',type);
-        $('#content').focus();
-        if(type !== content_type){
-            $('#content-type .option.active').removeClass('active');
-            $('#content-type .option[data-type="'+type+'"]').addClass('active');
-        }
-        content_type = type;
-    }
-    $('#content-type .option').click(function(){
-        set_content_type($(this).data('type'));
-    });
+    /* ----- Simple Text Editor ----- */
 
-    set_content_type('text');
-        
-    encoder = {};
-    encoder.text = function(){
+    function TextEditor($el,onupdate){
+        var  self = this;
+        this.el   = $el.get(0);
+        this.$el  = $(this.el);
+        this.onupdate = onupdate;
+        this.$el.bind('input propertychange',function(){ 
+            self.update(); 
+        });
+        this.update();
+        this.set_content('');
+    }
+
+    var proto = TextEditor.prototype;
+
+    proto.type     = 'text';
+    proto.datatype = 'string';
+
+    proto.show = function(){
+        this.$el.removeClass('hidden');
+        this.update();
+        this.$el.focus();
+    };
+    proto.hide = function(){
+        this.$el.addClass('hidden');
+    };
+    proto.has_content = function(){
+        return this.$el.val() !== "";
+    };
+    proto.set_content = function(txt){
+        this.$el.val(txt);
+    };
+    proto.get_content = function(){
+        return this.$el.val();
+    };
+    proto.update = function(){
+        if(this.onupdate){
+            this.onupdate(this);
+        }
+        this.el.style.height = 'auto';
+        this.el.style.height = this.el.scrollHeight + 'px';
+    };
+    proto.get_encoded_content = function(){
         // does txt.split(sep) then inserts the sep between the tokens
         function split(txt,sep){
             var out = [];
@@ -48,7 +73,7 @@ $(function(){
             return txt.replace(/[&<>"'\/]/g, function(s){ return entity_map[s]; });
         }
         
-        var content = $('#content').val();
+        var content = this.$el.val();
         var out = [];
         
         content = split(split(split(content,'\n'),'\t'),' ');
@@ -65,25 +90,212 @@ $(function(){
         return '<pre>'+out.join("")+'</pre>';
     };
 
-    encoder.html = function(){ 
-        return $('#content').val(); 
-    }; 
+    /* ----- HTML Text Editor ----- */
 
-    encoder.picture = function(){
-        return 'picture';
+    function HtmlEditor($el,onupdate){
+        var  self = this;
+        this.el   = $el.get(0);
+        this.$el  = $(this.el);
+        this.onupdate = onupdate;
+        this.editor = ace.edit(this.el);
+        this.editor.setTheme('ace/theme/pastel_on_dark');
+        this.editor.getSession().setMode('ace/mode/html');
+
+        this.editor.getSession().on('change',function(){
+            self.update(); 
+        });
+        this.set_content('');
+        this.update();
+    }
+
+    HtmlEditor.prototype = Object.create(TextEditor.prototype);
+
+    var proto = HtmlEditor.prototype;
+
+    proto.type     = 'html';
+    proto.datatype = 'string';
+
+    proto.update = function(){
+        if(this.onupdate){
+            this.onupdate(this);
+        }
+        var height = this.editor.getSession().getScreenLength() 
+                    * this.editor.renderer.lineHeight 
+                    + this.editor.renderer.scrollBar.getWidth();
+        height = Math.max(height,190);
+        console.log('height:',height);
+        this.$el.height(height.toString() + 'px');
+        this.editor.resize();
     };
+
+    proto.has_content = function(){
+        return this.editor.getValue() !== "";
+    };
+    proto.get_content = function(){
+        return this.editor.getValue();
+    };
+    proto.set_content = function(txt){
+        this.editor.setValue(txt);
+    };
+    proto.get_encoded_content = function(){
+        return this.editor.getValue();
+    };
+
+    /* ----- Image Uploader ----- */
+
+    function ImgEditor($el,onupdate){
+        var  self = this;
+        this.el   = $el.get(0);
+        this.$el  = $(this.el);
+        this.onupdate = onupdate;
+        this.update();
+        this.content = '';
+        this.$el.find('#img-selector').on('change',function(event){
+            self.loadimage(event.target.files[0]);
+        });
+    }
+
+    ImgEditor.prototype = Object.create(TextEditor.prototype);
+
+    var proto = ImgEditor.prototype;
+
+    proto.type     = 'img';
+    proto.datatype = 'img';
+
+    proto.update = function(){
+        if(this.onupdate){
+            this.onupdate(this);
+        }
+    };
+    proto.get_content = function(){
+        return this.content;
+    };
+    proto.get_encoded_content = function(){
+        return this.get_content();
+    };
+    proto.has_content = function(){
+        return this.content !== '';
+    };
+    proto.set_content = function(){};
+
+    proto.show_error = function(message){
+        $('#img-error').text(message);
+        $('#img-error').removeClass('hidden');
+    };
+    proto.hide_error = function(){
+        $('#img-error').addClass('hidden');
+    };
+    proto.loadimage = function(file){
+        var self = this;
+        this.hide_error();
+        if(!file.type.match(/image.*/)){
+            self.show_error('Unsupported File Format');
+            console.log('Not an image!');
+            return;
+        }
+
+        var reader = new FileReader();
+        reader.onload = function(event){
+            var img = new Image();
+            img.src = event.target.result;
+            self.content = '<img src="'+event.target.result+'">';
+            self.$el.find('.userimages').empty();
+            self.$el.find('.userimages').append(img);
+            self.$el.find('.dropinvite').addClass('hidden');
+            self.update();
+        };
+        reader.onerror = function(){
+            self.show_error('Could not read file');
+        };
+        reader.readAsDataURL(file);
+    };
+
+
+    /* ----- Main GUI ----- */
     
-    $('#content').bind('input propertychange',function(){
-        if($(this).val()){
+    function on_content_changed(editor){
+        if(editor.has_content()){
             $('#submit').removeClass('disabled');
         }else{
             $('#submit').addClass('disabled');
         }
         $('.urlbox').addClass('hidden');
+    }
+
+    var editors = {
+        'text': new TextEditor($('#editor-text'), on_content_changed),
+        'html': new HtmlEditor($('#editor-html'), on_content_changed),
+        'img' : new ImgEditor($('#editor-img'), on_content_changed),
+    };
+
+    var editor = editors.text;
+
+    function set_editor(type){
+        console.log('type:',type, editors);
+        if(editors[type] && editor.type !== type){
+            editor.hide();
+            if(editor.datatype === editors[type].datatype){
+                editors[type].set_content(editor.get_content());
+            }
+            editor = editors[type];
+            $('#content-type .option.active').removeClass('active');
+        }
+        $('#content-type .option[data-type="'+type+'"]').addClass('active');
+        editor.show();
+    }
+
+    set_editor('text');
+
+    $('#content-type .option').click(function(){
+        set_editor($(this).data('type'));
     });
+    
+    function display_security(security,score){
+        var s = $('#security');
+        if(security === 'insecure'){
+                s.addClass('invalid');
+                s.removeClass('valid');
+                s.html("<span class='progress'></span><i class='icon-warning-sign'></i>&nbsp; Insecure");
+                s.removeClass('hidden');
+                s.find('.progress').css('width',Math.floor(score*100)+'%');
+        }else if(security === 'secure'){
+                s.addClass('valid');
+                s.removeClass('invalid');
+                s.html("<span class='progress'></span><i class='icon-lock'></i>&nbsp; Secure");
+                s.removeClass('hidden');
+                s.find('.progress').css('width',Math.floor(score*100)+'%');
+        }else{
+                s.removeClass('valid');
+                s.addClass('hidden');
+                s.html('');
+        }
+    }
+
+    var timeout = null;
+    function checkPassword(){
+        var pw = $('#password');
+        clearTimeout(timeout);
+        timeout = setTimeout(function(){
+
+            if(pw.val().length === 0){
+                display_security('none');
+            }else{
+                var security = zxcvbn(pw.val(),['horsebatterystaple']);
+                console.log('entropy:',security.entropy);
+                if(security.entropy <= 64){
+                    display_security('insecure',security.entropy/64.0);
+                }else{
+                    display_security('secure',(security.entropy-64)/192.0);
+                }
+            }
+        },100);
+    }
+
+    $('#password').bind('change input propertychange',checkPassword);
+    $('#password').val('');
 
     $('#submit').click(function(){
-        var content = encoder[content_type](); 
+        var content = editor.get_encoded_content();
         console.log(content);
         var password = $('#password').val();
         if(content){ 

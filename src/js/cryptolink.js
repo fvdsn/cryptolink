d15bda02740f84ef93c6e78aa0567cdd0a201bc9
Frédéric van der Essen <fvdessen@gmail.com>
2013-08-10 01:42:21 +0200

    error when no data provided

--- src/js/cryptolink.js
+++ src/js/cryptolink.js
@@ -4,25 +4,39 @@
 
     cryptolink.version = "1.0";
 
-    var password_template = [
+    var header_template = [
         "<link href='http://fonts.googleapis.com/css?family=Source+Sans+Pro:200,300,400,600,700,900,200italic,300italic,400italic,600italic,700italic,900italic' rel='stylesheet' type='text/css'>",
         "<link rel='stylesheet' href='../css/main.css'>",
         "<style>",
             "html{ position: static; }",
-            ".pwbox{ width: 350px; height: 200px; position: absolute;",
+            ".centerbox{ width: 350px; height: 200px; position: absolute;",
                     "top: 50%; left: 50%; margin-top:-100px; margin-left:-175px;}",
-            ".pwbox h3{ font-size: 30px; margin: 30px 0px 8px; font-weight: 300; }",
+            ".centerbox h3{ font-size: 30px; margin: 30px 0px 8px; font-weight: 300; }",
+            ".centerbox.dark { padding: 0px 32px; box-sizing: border-box; border-radius: 3px; background: rgba(0, 0, 0, 0.11); }",
             ".bottomlink{ width: 350px; text-align: center; position: absolute; bottom: 0px;",
                     "left: 50%; margin-left:-175px; margin-bottom: 8px; text-decoration: none; color: rgb(120,120,120); }",
             
         "</style>",
-        "<div class='pwbox'>",
+        "",
+    ].join('\n');
+
+    var password_template = header_template + [
+        "<div class='centerbox'>",
             "<h3>Encrypted Document</h3>",
             "<input type='password' placeholder='Password' id='password' class='textinput'></input>",
             "<button id='decrypt' class='button disabled'>Decrypt</button>",
         "</div>",
         "<a class='bottomlink' href='/'>Encrypted with CryptoLink</a>",
     ].join('\n');
+
+    var empty_template = header_template + [
+        "<div class='centerbox dark'>",
+            "<h3>CryptoLink 1.0</h3>",
+            "<p>ERROR: Nothing to decrypt</p>",
+            "<p><a href='/'>Create a Cryptolink?</a></p>",
+        "</div>",
+    ].join('\n');
+
 
 
     function ask_for_password(pw_callback){
@@ -64,7 +78,7 @@
         if(window.location.hash){
             decode(window.location.hash.slice(1));
         }else{
-            $('html').html("<h1>Error: nothing to decrypt</h1>");
+            $('html').html(empty_template);
         }
     }
 
